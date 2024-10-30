3ee6c88ef2f35c99cff6a3aa3436737c70c0d9d5
Subhash kumar <148210135+12325670@users.noreply.github.com>
2024-10-30 19:03:02 +0530

    Add files via upload

---
+++ lec4_pattern.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cout << "Enter number";
+    // cin >> n;
+
+    // int i=1;
+    // while (i<=n){
+    //     int j=1;
+    //     while (j<=n){
+    //     cout << j;
+    //     j=j+1;
+    // }
+    // cout << endl;
+    // i=i+1;
+    // }
+
+
+    //-------------pattern 1 ---------------------
+   // cout << "enter number of rows ";
+
+    // int n;
+    // cin >> n;
+    // int i=0;
+
+    // while( i < n){
+
+    //     int j=0;
+    //     while(j<n){
+    //     cout << "* " ;
+    //     j= j+1;
+    //     }
+    //     cout << endl;
+    // i=i+1;    
+    
+    // }
+
+
+    // //------------------rows and coloum pattern project1----------------
+    // int r ,c ;
+    // cout << "enter number of rows: ";
+    // cin >> r;
+    // cout << "enter number of coloumns: ";
+    // cin >> c;
+    // int i=1;
+    // while(i<=r){
+    //     int j=1;
+    //     while (j<=c){
+    //         cout << " * ";
+    //         j=j+1;
+    //     }
+        
+    //     cout << endl;
+    //     i= i+1;
+
+    // }
+
+    
+    return 0;
+}
